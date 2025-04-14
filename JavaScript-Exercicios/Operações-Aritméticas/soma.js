5a3dce4be311ec44911d6c0366d72260636ea225
anasant0ss <167447632+anasant0ss@users.noreply.github.com>
2025-04-13 22:40:11 -0300

    Add files via upload

---
+++ JavaScript-Exercicios/Operações-Aritméticas/soma.js
@@ -0,0 +1,5 @@
+alert("Vamos somar?!")
+let numero1 = parseFloat(prompt("Digite um valor:"));
+let numero2 = parseFloat(prompt("Digite outro valor:"));
+let soma = numero1 + numero2;
+alert("O resultado de: " + numero1 + " + " + numero2 + " é igual a: " + soma);
