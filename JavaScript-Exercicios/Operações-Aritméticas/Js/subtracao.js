e37e7e242dc2a90543f874c30249417b7abd2db4
anasant0ss <167447632+anasant0ss@users.noreply.github.com>
2025-04-13 22:39:38 -0300

    Delete JavaScript-Exercicios/Operações-Aritméticas/Js directory

--- JavaScript-Exercicios/Operações-Aritméticas/Js/subtracao.js
+++
@@ -1,5 +0,0 @@
-alert("Vamos Subtrair?!")
-let numero1 = parseFloat(prompt("Digite um valor:"));
-let numero2 = parseFloat(prompt("Digite outro valor:"));
-let soma = numero1 + numero2;
-alert("O resultado de: " + numero1 + " - " + numero2 + " é igual a: " + soma);
